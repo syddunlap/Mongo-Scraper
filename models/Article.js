fc2421c2679ebb020c8cf3004ff5c73e17b5ec78
Sydney Dunlap <sydney.dunlap2011@gmail.com>
2019-03-12 07:18:34 -0400

    trying a lot of things that don't work but changed up the file skeleton a little bit

--- models/Article.js
+++ models/Article.js
@@ -15,11 +15,19 @@ const ArticleSchema = new Schema({
   link: {
     type: String,
     required: true
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  node: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
   }
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+const Article = mongoose.model("Article", ArticleSchema);
 
 // Export the Article model
 module.exports = Article;
--- server.js
+++ server.js
@@ -1,42 +1,46 @@
-// Dependencies
 const express = require("express");
-const exphbs = require("express-handlebars");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
 
 // Scraping tools
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-// Require all models
+// Require models
 const db = require("./models");
-
-// Initialize express
-const app = express();
-
-// Start PORT
 const PORT = process.env.PORT || 3000;
 
-// Configure Middleware
-app.use(express.urlencoded({ extended: true }));
+// Initialize Express
+const app = express();
+
+// Configure middleware
+app.use(logger("dev"));
 // Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+// Connect to the Mongo DB
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 // Handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
 // Routes
-// require("./routes/routes")(app);
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
-// Connect to the MongoDB
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-// Start the Server
-app.listen(PORT, () => {
-    console.log("App running on Port " + PORT + ".");
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
-
-module.exports = app;
